44aae33fd17826c7e3bd4e96e368cae235bf0354
Fernando <fdofuentescarrasco@gmail.com>
2025-06-30 10:02:10 +0200

    Ignore warnings

--- scraper/dentalexpress.py
+++ scraper/dentalexpress.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver # type: ignore
+from selenium.webdriver.chrome.options import Options # type: ignore
+from selenium.webdriver.common.by import By # type: ignore 
+from selenium.webdriver.support.ui import WebDriverWait # type: ignore
+from selenium.webdriver.support import expected_conditions as EC # type: ignore
 import time
 import unicodedata
 
